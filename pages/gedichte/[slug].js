aa8d9f32197862f1517040b6b7f67566f471cbab
fvjupiter <schoof.frederik@gmail.com>
2022-09-08 21:44:11 +0200

    next prev buttons poems

--- pages/gedichte/[slug].js
+++ pages/gedichte/[slug].js
@@ -1,8 +1,10 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { createClient } from 'contentful'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import { INLINES } from '@contentful/rich-text-types';
 import Link from 'next/link';
+import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
+import Loader from '../../components/Loader';
 
 const richText_Options = {
   renderNode: {
@@ -10,34 +12,73 @@ const richText_Options = {
   },
 }
 
-export default function Gedicht({ slug, poems, setpoems, gedichte, setinfo, information }) {
+export default function Gedicht({ screen, slug, poems, setpoems, gedichte, setinfo, information }) {
   
   useEffect(() => {
     gedichte && setpoems(gedichte)
     information && setinfo(information)
   }, [gedichte, information])
 
-  return (
+  const [isLoading, setisLoading] = useState(false)
+  const [poem, setpoem] = useState(null)
+  useEffect(() => {
+    const poem = poems.find(poem => poem.fields.slug === slug)
+    setpoem(poem)
+    setisLoading(false)
+  }, [slug, poems])
+
+  const cNButtons = `text-3xl lg:text-5xl text-pink-900 hover:text-pink-600 duration`
+  // create a function which uses next/link to navigate to the previous poem
+  const PrevPoem = () => {
+    const index = poems.findIndex(poem => poem.fields.slug === slug)
+    const prevPoem = poems[index - 1]
+    if (prevPoem) {
+      return <Link href={`/gedichte/${prevPoem.fields.slug}`}><a onClick={() => setisLoading(true)} className={`${cNButtons} left-0`}><BsChevronLeft/></a></Link>
+    } else {
+      return <Link href={`/gedichte/${poems[poems.length - 1].fields.slug}`}><a onClick={() => setisLoading(true)} className={`${cNButtons} left-0`}><BsChevronLeft/></a></Link>
+    }
+  }
+
+  // create a function which uses next/link to navigate to the next poem
+  const NextPoem = () => {
+    const index = poems.findIndex(poem => poem.fields.slug === slug)
+    const nextPoem = poems[index + 1]
+    if (nextPoem) {
+      return <Link href={`/gedichte/${nextPoem.fields.slug}`}><a onClick={() => setisLoading(true)} className={`${cNButtons} right-0`}><BsChevronRight/></a></Link>
+    } else {
+      return <Link href={`/gedichte/${poems[0].fields.slug}`}><a onClick={() => setisLoading(true)} className={`${cNButtons} right-0`}><BsChevronRight/></a></Link>
+    }
+  }
+
+  if(poem) return (
     <div className='mb-40 px-4 lg:px-24'>
-        <div className='py-20 text-pink-900 text-center'>
-          <h1 className='text-4xl sm:text-5xl font-semibold text-center mb-4'>{gedichte[0].fields.title}</h1>
-          <div className='text-xl font-normal'>{gedichte[0].fields.datum}</div>
+        <div className='relative py-20 text-pink-900 text-center'>
+          <div className='between mb-4'>
+            <PrevPoem />
+            {!isLoading
+            ? <h1 className='text-2xl lg:text-5xl font-semibold text-center'>{poem.fields.title}</h1>
+            : <Loader />}
+            <NextPoem />
+          </div>
+          <div className='text-xl font-normal'>{poem.fields.datum}</div>
+          
           {/* <div>{documentToReactComponents(info.descriptionpoems, richText_Options)}</div> */}
         </div>
       {gedichte &&
         <div className={`
           w-full sm:w-11/12 max-w-[750px] m-6 mt-0 mb-12 p-4 mx-auto
-          text-center text-xl
+          text-center text-sm sm:text-base md:text-sm lg:text-xl
           bg-gradient-to-b rounded-lg border ring-1
           from-white to-pink-50 border-pink-400 ring-pink-900 text-pink-900
           shadow-2xl
           `}>
           {/* <div className='text-base font-normal'>{gedichte[0].fields.datum}</div> */}
-          <div>{documentToReactComponents(gedichte[0].fields.content, richText_Options)}</div>
+          <div>{documentToReactComponents(poem.fields.content, richText_Options)}</div>
         </div>
       }
     </div>
   )
+  return <div className='h-screen w-full center'><Loader /></div>
 }
 
 const client = createClient({
--- pages/kurzgeschichten/[slug].js
+++ pages/kurzgeschichten/[slug].js
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { createClient } from 'contentful'
 import Loader from '../../components/Loader'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import { INLINES } from '@contentful/rich-text-types';
 import Link from 'next/link';
+import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
 
 const richText_Options = {
   renderNode: {
@@ -18,11 +19,44 @@ export default function Story({ slug, stories, setstories, kurzgeschichten, seti
     information && setinfo(information)
   }, [kurzgeschichten])
 
-  if(kurzgeschichten) return (
+  const [story, setstory] = useState(null)
+  useEffect(() => {
+    const story = stories.find(story => story.fields.slug === slug)
+    setstory(story)
+  }, [slug, stories])
+
+  const cNButtons = `text-3xl lg:text-5xl text-blue-900 hover:text-blue-600 duration`
+  // create a function which uses next/link to navigate to the previous story
+  const PrevStory = () => {
+    const index = stories.findIndex(story => story.fields.slug === slug)
+    const prevStory = stories[index - 1]
+    if (prevStory) {
+      return <Link href={`/kurzgeschichten/${prevStory.fields.slug}`}><a className={`${cNButtons} left-0`}><BsChevronLeft/></a></Link>
+    } else {
+      return <Link href={`/kurzgeschichten/${stories[stories.length - 1].fields.slug}`}><a className={`${cNButtons} left-0`}><BsChevronLeft/></a></Link>
+    }
+  }
+
+  // create a function which uses next/link to navigate to the next story
+  const NextStory = () => {
+    const index = stories.findIndex(story => story.fields.slug === slug)
+    const nextStory = stories[index + 1]
+    if (nextStory) {
+      return <Link href={`/kurzgeschichten/${nextStory.fields.slug}`}><a className={`${cNButtons} right-0`}><BsChevronRight/></a></Link>
+    } else {
+      return <Link href={`/kurzgeschichten/${stories[0].fields.slug}`}><a className={`${cNButtons} right-0`}><BsChevronRight/></a></Link>
+    }
+  }
+
+  if(story) return (
     <div className='mb-40 px-4 lg:px-24'>
         <div className='py-20 text-blue-900 text-center'>
-          <h1 className='text-4xl sm:text-5xl font-semibold text-center mb-4'>{kurzgeschichten[0].fields.title}</h1>
-          <div className='text-xl font-normal'>{kurzgeschichten[0].fields.dateText}</div>
+          <div className='between mb-4'>
+            <PrevStory />
+            <h1 className='text-2xl lg:text-5xl font-semibold text-center'>{story.fields.title}</h1>
+            <NextStory />
+          </div>
+          <div className='text-xl font-normal'>{story.fields.dateText}</div>
         </div>
       <div className={`
           w-full sm:w-11/12 max-w-[1280px] m-6 mt-0 mb-12 p-4 mx-auto
@@ -31,7 +65,7 @@ export default function Story({ slug, stories, setstories, kurzgeschichten, seti
           from-white to-blue-50 border-blue-400 ring-blue-900 text-blue-900
           shadow-2xl
           `}>
-          <div>{documentToReactComponents(kurzgeschichten[0].fields.content, richText_Options)}</div>
+          <div>{documentToReactComponents(story.fields.content, richText_Options)}</div>
         </div>
     </div>
   )
