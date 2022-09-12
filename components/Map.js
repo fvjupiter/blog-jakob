16aafa436942d3a219fa8e5b58db3749a4940dcb
fvjupiter <schoof.frederik@gmail.com>
2022-09-12 16:41:14 +0200

    map loader

--- components/Map.js
+++ components/Map.js
@@ -1,5 +1,6 @@
 import { useEffect, useRef } from "react";
 import { BsX } from "react-icons/bs";
+import Loader from "./Loader";
 
 export default function Map({ isMapOpen, setIsMapOpen, loc }) {
 
@@ -27,9 +28,10 @@ export default function Map({ isMapOpen, setIsMapOpen, loc }) {
             <div ref={topRef} className={`absolute top-0`}/>
             <div className={`text-center h-full w-full`}>
                 <BsX className="fixed left-0 top-0 mt-2 ml-2 cursor-pointer text-gray-300 hover:text-white" onClick={() => setIsMapOpen(false)} size={30}/>
+                <div className='absolute h-full w-full center'><Loader /></div>
                 {loc.lat && loc.lon 
                 && <iframe
-                    className={`w-full h-full`}
+                    className={`absolute w-full h-full z-10`}
                     src={`https://maps.google.com/maps?q=${loc.lat},${loc.lon}&t=&z=13&ie=UTF8&iwloc=&output=embed`}
                 />}
             </div>
